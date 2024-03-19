3a7316e7f154492204c573b5d9383c24500be30a
JavierNIS <javierni.sanchez@gmail.com>
2024-03-19 18:33:43 +0100

    UTF-8 completado

--- source/conversionUTF8.cpp
+++ source/conversionUTF8.cpp
@@ -16,8 +16,11 @@ CharLength(const charUTF8_t* src, const std::size_t& max){
         return 0;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 std::size_t 
-UTF8toUTF16(const charUTF8_t* src, charUTF16_t* dest, conversionstate_t* conver, const std::size_t& max){
+UTF8toUTF16(const charUTF8_t* src, charUTF16_t* dest, conversionstate_t* conver, 
+        const std::size_t& max){
     if(dest == 0 || conver == 0)
         return 0;
     switch (CharLength(src, max)){
@@ -25,34 +28,92 @@ UTF8toUTF16(const charUTF8_t* src, charUTF16_t* dest, conversionstate_t* conver,
             return 0;
             break;
         case 1:
-            if(max >= 1)
+            if(max >= 1){
                 *dest = *src;
-            else
-                return 0;
-            return 1;
+                return 1;
+            }
             break;
         case 2:
-            if(max >= 2)
-                *dest = ((src[0] & 0x1F) << 5 | (src[1] & 0x3F));
-            else
-                return 0;
-            return 2;
+            if(max >= 2){
+                *dest = ((src[1] & 0x3F) |
+                        (src[0] & 0x1F) << 6);
+                return 2;
+            }
             break;
         case 3:
-            if(max >= 3)
-                *dest = ((src[2] & 0x3F) | (src[1] & 0x3F) << 6 | (src[0] & 0x0F) << 12);
+            if(max >= 3){
+                *dest = ((src[2] & 0x3F) | 
+                        (src[1] & 0x3F) << 6 | 
+                        (src[0] & 0x0F) << 12);
+                return 3;
+            }
         case 4:
             if(max == 4){
-                dest[1] = ((src[3] & 0x3F) | (src[2] & 0x0F) << 6);
-                dest[0] = ((src[2] & 0x30) | ((src[1] -0x10000) & 0x3F) << 2 | (src[0] & 0x07) << 7);
-            }else 
-                return 0;
-            return 4;
+                dest[1] = ( 0xDC00 | 
+                        (src[3] & 0x3F) | 
+                        (src[2] & 0x0F) << 6);
+                dest[0] = ( 0xD800 | 
+                        (src[2] & 0x30) >> 4 | 
+                        ((src[1] -0x10) & 0x3F) << 2 | 
+                        (src[0] & 0x07) << 8);
+                return 4;
+            } 
             break;
-            
+    }
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 std::size_t 
-UTF8toUTF32(const charUTF8_t* src, charUTF32_t* dest, conversionstate_t* conver, const std::size_t& max);
+UTF8toUTF32(const charUTF8_t* src, charUTF32_t* dest, conversionstate_t* conver, const std::size_t& max){
+    if(dest == 0 || conver == 0)
+        return 0;
+    switch (CharLength(src, max)){
+        case 0:
+            return 0;
+            break;
+        case 1:
+            if(max >= 1){
+                *dest = *src;
+                return 1;
+            }
+            break;
+        case 2:
+            if(max >= 2){
+                *dest = ((src[1] & 0x3F) |
+                        (src[0] & 0x1F) << 6);
+                return 2;
+            }
+            break;
+        case 3:
+            if(max >= 3){
+                *dest = ((src[2] & 0x3F) |
+                        (src[1] & 0x3F) << 6 |
+                        (src[0] & 0x0F) << 12);
+                return 3;
+            }
+            break;
+        case 4:
+            if(max == 4){
+                *dest = ((src[3] & 0x3F) |
+                        (src[2] & 0x3F) << 6 |
+                        (src[1] & 0x3F) << 12 |
+                        (src[0] & 0x07) << 18);
+                return 4;
+            }
+            break;
+    }
+    return 0;
+}
+                        
+////////////////////////////////////////////////////////////////////////////////
 
 std::size_t 
-UTF8toWIDE(const charUTF8_t* src, widechar_t* dest, conversionstate_t* conver, const std::size_t& max);
+UTF8toWIDE(const charUTF8_t* src, widechar_t* dest, conversionstate_t* conver, const std::size_t& max){
+#ifdef _WIN32
+    return UTF8toUTF16(src, dest, conver, max);
+#elif defined __linux__
+    return UTF8toUTF32(src, dest, conver, max);
+#endif
+}
--- source/main.cpp
+++ source/main.cpp
@@ -18,7 +18,46 @@ int main (int argc, char *argv[]) {
     for(int i = 0; i < size; i++)
         cin >> caracter[i];
 
-    cout << "El tamaño es: " << CharLength(caracter, size) << endl;
+    size_t tamañoreal = CharLength(caracter, size);
+    cout << "El tamaño es: " << tamañoreal << endl << endl;
+    
+    charUTF16_t* utf16caracter = 0;
+    if(tamañoreal == 4)
+        utf16caracter = new charUTF16_t[2];
+    else
+        utf16caracter = new charUTF16_t;
+    conversionstate_t conver;
+    Initialize(&conver);
+    
+    cout << "Convirtiendo a UTF16: " << endl << endl;
 
+    if(UTF8toUTF16(caracter, utf16caracter, &conver, size) != tamañoreal){
+        cerr << "problema en conversión a utf16" << endl;
+        return -1;
+    }
+
+    cout << "En hexadecimal: " << hex << *utf16caracter; 
+
+    if(tamañoreal == 4)
+        cout << utf16caracter[1];
+    cout << endl;
+    cout << "caracter: " << dec << *utf16caracter << endl << endl;
+
+    charUTF32_t utf32caracter;
+
+    cout << "Convirtiendo a UTF32: " << endl << endl;
+
+    if(UTF8toUTF32(caracter, &utf32caracter, &conver, size) != tamañoreal){
+        cerr << "problema en conversión a utf16" << endl;
+        return -1;
+    }
+
+    cout << "En hexadecimal: " << hex << utf32caracter << endl;
+    cout << "caracter: " << dec << utf32caracter << endl << endl;
+
+    if(tamañoreal == 4)
+        delete [] utf16caracter;
+    else
+        delete utf16caracter;
     return 0;
 }
